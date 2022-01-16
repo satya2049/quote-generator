bac11998225e5f0a9fe04b60b7a88376871c8bd2
satyaperumalla <satyaperumalla09@gmail.com>
2022-01-16 21:21:04 +0530

    added files

---
+++ index.js
@@ -0,0 +1,50 @@
+let quoteText = document.getElementById('quote-text');
+let authorName = document.getElementById('author-name');
+let newQuoteBtn = document.getElementById('new-quote');
+let twitterBtn = document.getElementById('twitter-icon');
+let loader = document.getElementById('loader');
+let quoteContainer = document.getElementById('quote-container');
+let apiQuotes = [];
+
+function showSpinner(){
+    loader.hidden = false;
+    quoteContainer.hidden = true
+}
+function HideSpinner(){
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
+function newQuote(){
+    showSpinner();
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+    quote.text.length > 50 ? quoteText.classList.add('long-quote'):
+    quoteText.classList.remove('long-quote');
+    quoteText.innerHTML = quote.text;
+    authorName.innerHTML = quote.author.length ? quote.author : 'unKnown';
+    HideSpinner();
+}
+async function quoteGenerator(){
+    showSpinner();
+    try{
+    const response = await fetch('https://type.fit/api/quotes');
+    apiQuotes = await response.json();
+    newQuote();
+    }
+    catch(error){
+        console.error(error);
+    }
+}
+
+
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerHTML} - ${authorName.innerHTML}`;
+    window.open(twitterUrl,'_blank');
+}
+quoteGenerator();
+
+//EventListeners
+newQuoteBtn.addEventListener('click',newQuote);
+twitterBtn.addEventListener('click',tweetQuote);
+
